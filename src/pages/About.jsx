3cbfaf0a4f0420fa824d5fe2b5f7039f3bb47618
Teddy <tedloukas12@gmail.com>
2023-03-30 16:06:01 -0400

    Add tooltips to logos

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,6 +14,97 @@ import framerLogo from "../assets/images/framer-motion-logo.png";
 import reduxLogo from "../assets/images/redux-logo.png";
 import webpackLogo from "../assets/images/webpack-logo.png";
 import teddyPhoto from "../assets/images/teddy-profile.jpg";
+import { Button, ButtonToolbar, Popover, Whisper } from "rsuite";
+
+//Popover content
+
+const HtmlSpeaker = (
+  <Popover title="HTML5">
+    <p>A markup language used to structure and present content</p>
+  </Popover>
+);
+
+const CSSSpeaker = (
+  <Popover title="CSS3">
+    <p>
+      A style sheet language used to describe the presentation of a document
+    </p>
+  </Popover>
+);
+const JSSpeaker = (
+  <Popover title="Javascript">
+    <p>A programming language that conforms to the ECMAScript specification</p>
+  </Popover>
+);
+const ReactSpeaker = (
+  <Popover title="React">
+    <p>A JavaScript library for building user interfaces</p>
+  </Popover>
+);
+const NodeSpeaker = (
+  <Popover title="Node.js">
+    <p>
+      An open-source, cross-platform, back-end JavaScript runtime environment
+    </p>
+  </Popover>
+);
+const ViteSpeaker = (
+  <Popover title="Vite">
+    <p>
+      A web development build tool that serves your code via native ES Module
+      imports during development
+    </p>
+  </Popover>
+);
+const ExpressSpeaker = (
+  <Popover title="Express.js">
+    <p>A web application framework for Node.js</p>
+  </Popover>
+);
+const WordpressSpeaker = (
+  <Popover title="Wordpress">
+    <p>
+      A free and open-source content management system written in PHP and paired
+      with a MySQL or MariaDB database
+    </p>
+  </Popover>
+);
+const NextSpeaker = (
+  <Popover title="Next.js">
+    <p>
+      A free and open-source React-based web framework that enables
+      functionality such as server-side rendering and generating static websites
+      for React based web applications.
+    </p>
+  </Popover>
+);
+const FramerSpeaker = (
+  <Popover title="Framer Motion">
+    <p>
+      A popular open-source animation library for React that makes it easy to
+      create smooth and interactive animations, transitions, and gestures
+    </p>
+  </Popover>
+);
+const ReduxSpeaker = (
+  <Popover title="Redux">
+    <p>
+      A predictable state management library for JavaScript applications that
+      simplifies the management of application state in a single immutable
+      store.
+    </p>
+  </Popover>
+);
+const WebpackSpeaker = (
+  <Popover title="Webpack">
+    <p>
+      A module bundler for JavaScript applications that processes and optimizes
+      code and assets
+    </p>
+  </Popover>
+);
+
+//About page component
 
 export default function About(props) {
   return (
@@ -25,6 +116,7 @@ export default function About(props) {
       ref={props.aboutRef}
     >
       <h2 className="about__title">About</h2>
+
       <div className="about__content">
         <img src={teddyPhoto} alt="nothing" className="content__picture" />
         <h2 className="content__name">Teddy Loukas</h2>
@@ -40,115 +132,139 @@ export default function About(props) {
         <div className="content__skills-group">
           <h3 className="skills-group__title">Skills</h3>
           <motion.div className="skills-group__skill">
-            <motion.img
-              initial={{ scale: 0 }}
-              transition={{ duration: 1, type: "spring" }}
-              animate={{ scale: 1 }}
-              className="skill__image"
-              src={htmlLogo}
-              alt="HTML5"
-              title="HTML5"
-            />
-            <motion.img
-              initial={{ scale: 0 }}
-              transition={{ duration: 1, type: "spring", delay: 0.1 }}
-              animate={{ scale: 1 }}
-              className="skill__image"
-              src={cssLogo}
-              alt="CSS3"
-              title="CSS3"
-            />
+            <Whisper placement="bottom" trigger="hover" speaker={HtmlSpeaker}>
+              <motion.img
+                initial={{ scale: 0 }}
+                transition={{ duration: 1, type: "spring" }}
+                animate={{ scale: 1 }}
+                className="skill__image"
+                src={htmlLogo}
+                alt="HTML5"
+              />
+            </Whisper>
+            <Whisper placement="bottom" trigger="hover" speaker={CSSSpeaker}>
+              <motion.img
+                initial={{ scale: 0 }}
+                transition={{ duration: 1, type: "spring", delay: 0.1 }}
+                animate={{ scale: 1 }}
+                className="skill__image"
+                src={cssLogo}
+                alt="CSS3"
+              />
+            </Whisper>
 
-            <motion.img
-              initial={{ scale: 0 }}
-              transition={{ duration: 1, type: "spring", delay: 0.2 }}
-              animate={{ scale: 1 }}
-              className="skill__image"
-              src={jsLogo}
-              alt="Javascript"
-              title="Javascript"
-            />
-            <motion.img
-              initial={{ scale: 0 }}
-              transition={{ duration: 1, type: "spring", delay: 0.3 }}
-              animate={{ scale: 1 }}
-              className="skill__image"
-              src={reactLogo}
-              alt="React"
-              title="React"
-            />
-            <motion.img
-              initial={{ scale: 0 }}
-              transition={{ duration: 1, type: "spring", delay: 0.4 }}
-              animate={{ scale: 1 }}
-              className="skill__image"
-              src={nodeLogo}
-              alt="Node.js"
-              title="Node.js"
-            />
-            <motion.img
-              initial={{ scale: 0 }}
-              transition={{ duration: 1, type: "spring", delay: 0.5 }}
-              animate={{ scale: 1 }}
-              className="skill__image"
-              src={viteLogo}
-              alt="Vite"
-              title="Vite"
-            />
-            <motion.img
-              initial={{ scale: 0 }}
-              transition={{ duration: 1, type: "spring", delay: 0.6 }}
-              animate={{ scale: 1 }}
-              className="skill__image"
-              src={expressLogo}
-              alt="Express.js"
-              title="Express.js"
-            />
-            <motion.img
-              initial={{ scale: 0 }}
-              transition={{ duration: 1, type: "spring", delay: 0.7 }}
-              animate={{ scale: 1 }}
-              className="skill__image"
-              src={wordpressLogo}
-              alt="Wordpress"
-              title="Wordpress"
-            />
-            <motion.img
-              initial={{ scale: 0 }}
-              transition={{ duration: 1, type: "spring", delay: 0.8 }}
-              animate={{ scale: 1 }}
-              className="skill__image"
-              src={framerLogo}
-              alt="Framer Motion"
-              title="Framer Motion"
-            />
-            <motion.img
-              initial={{ scale: 0 }}
-              transition={{ duration: 1, type: "spring", delay: 0.9 }}
-              animate={{ scale: 1 }}
-              className="skill__image"
-              src={nextLogo}
-              alt="Next.js"
-              title="Next.js"
-            />
-            <motion.img
-              initial={{ scale: 0 }}
-              transition={{ duration: 1, type: "spring", delay: 1 }}
-              animate={{ scale: 1 }}
-              className="skill__image"
-              src={reduxLogo}
-              alt="Redux"
-              title="Redux"
-            />
-            <motion.img
-              initial={{ scale: 0 }}
-              transition={{ duration: 1, type: "spring", delay: 1.1 }}
-              animate={{ scale: 1 }}
-              className="skill__image"
-              src={webpackLogo}
-              alt="Webpack"
-              title="Webpack"
-            />
+            <Whisper placement="bottom" trigger="hover" speaker={JSSpeaker}>
+              <motion.img
+                initial={{ scale: 0 }}
+                transition={{ duration: 1, type: "spring", delay: 0.2 }}
+                animate={{ scale: 1 }}
+                className="skill__image"
+                src={jsLogo}
+                alt="Javascript"
+              />
+            </Whisper>
+            <Whisper placement="bottom" trigger="hover" speaker={ReactSpeaker}>
+              <motion.img
+                initial={{ scale: 0 }}
+                transition={{ duration: 1, type: "spring", delay: 0.3 }}
+                animate={{ scale: 1 }}
+                className="skill__image"
+                src={reactLogo}
+                alt="React"
+              />
+            </Whisper>
+            <Whisper placement="bottom" trigger="hover" speaker={NodeSpeaker}>
+              <motion.img
+                initial={{ scale: 0 }}
+                transition={{ duration: 1, type: "spring", delay: 0.4 }}
+                animate={{ scale: 1 }}
+                className="skill__image"
+                src={nodeLogo}
+                alt="Node.js"
+              />
+            </Whisper>
+            <Whisper placement="bottom" trigger="hover" speaker={ViteSpeaker}>
+              <motion.img
+                initial={{ scale: 0 }}
+                transition={{ duration: 1, type: "spring", delay: 0.5 }}
+                animate={{ scale: 1 }}
+                className="skill__image"
+                src={viteLogo}
+                alt="Vite"
+              />
+            </Whisper>
+            <Whisper
+              placement="bottom"
+              trigger="hover"
+              speaker={ExpressSpeaker}
+            >
+              <motion.img
+                initial={{ scale: 0 }}
+                transition={{ duration: 1, type: "spring", delay: 0.6 }}
+                animate={{ scale: 1 }}
+                className="skill__image"
+                src={expressLogo}
+                alt="Express.js"
+              />
+            </Whisper>
+            <Whisper
+              placement="bottom"
+              trigger="hover"
+              speaker={WordpressSpeaker}
+            >
+              <motion.img
+                initial={{ scale: 0 }}
+                transition={{ duration: 1, type: "spring", delay: 0.7 }}
+                animate={{ scale: 1 }}
+                className="skill__image"
+                src={wordpressLogo}
+                alt="Wordpress"
+              />
+            </Whisper>
+            <Whisper placement="bottom" trigger="hover" speaker={FramerSpeaker}>
+              <motion.img
+                initial={{ scale: 0 }}
+                transition={{ duration: 1, type: "spring", delay: 0.8 }}
+                animate={{ scale: 1 }}
+                className="skill__image"
+                src={framerLogo}
+                alt="Framer Motion"
+              />
+            </Whisper>
+            <Whisper placement="bottom" trigger="hover" speaker={NextSpeaker}>
+              <motion.img
+                initial={{ scale: 0 }}
+                transition={{ duration: 1, type: "spring", delay: 0.9 }}
+                animate={{ scale: 1 }}
+                className="skill__image"
+                src={nextLogo}
+                alt="Next.js"
+              />
+            </Whisper>
+            <Whisper placement="bottom" trigger="hover" speaker={ReduxSpeaker}>
+              <motion.img
+                initial={{ scale: 0 }}
+                transition={{ duration: 1, type: "spring", delay: 1 }}
+                animate={{ scale: 1 }}
+                className="skill__image"
+                src={reduxLogo}
+                alt="Redux"
+              />
+            </Whisper>
+            <Whisper
+              placement="bottom"
+              trigger="hover"
+              speaker={WebpackSpeaker}
+            >
+              <motion.img
+                initial={{ scale: 0 }}
+                transition={{ duration: 1, type: "spring", delay: 1.1 }}
+                animate={{ scale: 1 }}
+                className="skill__image"
+                src={webpackLogo}
+                alt="Webpack"
+              />
+            </Whisper>
           </motion.div>
         </div>
       </div>
